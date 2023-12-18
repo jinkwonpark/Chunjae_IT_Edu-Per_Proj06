217da51cc35c231afeb0b06ea03d265457f0efff
jinkwonpark <jk3473@naver.com>
2023-12-18 17:47:04 +0900

    login페이지 작성

---
+++ src/main/java/com/pro06/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.pro06.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/login")
+    public String loginForm(){
+        return "member/login";
+    }
+}
