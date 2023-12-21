6a33271d365eee3c213831e8931c10ea81b33437
jinkwonpark <jk3473@naver.com>
2023-12-21 14:30:02 +0900

    회원, 공지사항 관련 view 제작중

--- src/main/java/com/pro06/controller/MemberController.java
+++ src/main/java/com/pro06/controller/MemberController.java
@@ -1,20 +1,38 @@
 package com.pro06.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
 
+//    private MemberService memberService;
+
+    // 로그인
     @GetMapping("/login")
     public String loginForm(){
         return "member/login";
     }
 
+    // 회원약관
     @GetMapping("/term")
     public String Term(){
         return "member/term";
     }
+
+    // 회원가입
+    @GetMapping("/join")
+    public String joinForm(){
+        return "member/join";
+    }
+
+//    @PostMapping("/join")
+//    public String join(){
+//        return "redirect:/member/login";
+//    }
 }
